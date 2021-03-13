31e16988cb43db12ca4493dcb0a972721d0d5340
Aniket Jaiswal <IIT2019085@iiita.ac.in>
2021-03-13 17:21:04 +0530

    Code initialized

---
+++ soe/android/app/src/main/kotlin/com/example/soe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.soe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ soe/lib/screens/sensor_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Sensor_data extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
