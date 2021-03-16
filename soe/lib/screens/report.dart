212112fb602352dc58835d83962c8eb0f9f3e467
Aniket Jaiswal <IIT2019085@iiita.ac.in>
2021-03-16 17:24:33 +0530

    Report screen updated

--- soe/lib/screens/report.dart
+++ soe/lib/screens/report.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:soe/constants.dart';
+
+import '../components.dart';
 
 class Report extends StatefulWidget {
   @override
@@ -12,68 +16,105 @@ class _ReportState extends State<Report> {
     "Some of sensors needs servicing.",
     "Hello I am Aniket Jaiswal.\nSecond year B.Tech.\nTech Enthu...\nCompetiti...\nYep\nIt's me"
   ];
+  String currentreport;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  "Past Reports",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  height: MediaQuery.of(context).size.height * .4,
-                  child: ListView.builder(
-                    itemCount: reports.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          padding: EdgeInsets.all(30),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Past Reports   ",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'Pacifico',
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * .4,
+                      child: ListView.builder(
+                        itemCount: reports.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              padding: EdgeInsets.all(30),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20),
+                                  ),
+                                  color: kBlue),
+                              child: ListView(
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: reports[index],
+                                      style: TextStyle(
+                                          fontSize: 15, color: Colors.white),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              color: kBlue),
-                          child: ListView(
-                            children: [
-                              RichText(
-                                text: TextSpan(
-                                  text: reports[index],
-                                  style: TextStyle(
-                                      fontSize: 15, color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                          height: 150,
-                          width: 150,
+                              height: 150,
+                              width: 150,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: kBlue,
+                        hoverColor: kBlue,
+                        focusColor: kBlue,
+                        labelText: "Your current report...",
+                        hintText: "Type here...",
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: kBlue, width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
                         ),
-                      );
-                    },
+                      ),
+                      maxLines: 10,
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: RoundedButton(
+                      buttonColor: kBlueColor,
+                      buttonTitle: 'Send Report',
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
